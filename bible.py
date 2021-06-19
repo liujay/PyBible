1d9e0c9ed52a793d1d342faf3b1ee3d228789e75
Jay Liu <jay.s.liu@gmail.com>
2021-06-19 17:19:22 -0400

    update to python3

--- bible.py
+++ bible.py
@@ -23,10 +23,10 @@ import random, re
 
 def random_verse(bible, book=False):
   if not book:
-     book = random.choice(bible.keys())
-  chapter = random.choice(bible[book].keys())
-  verse = random.choice(bible[book][chapter].keys())
-  return '%s %s:%s \n%s' % (book, chapter, verse, bible[book][chapter][verse])
+     book = random.choice(list(bible.keys()))
+  chapter = random.choice(list(bible[book].keys()))
+  verse = random.choice(list(bible[book][chapter].keys()))
+  return '{0} {1}:{2} \n{3}'.format(book, chapter, verse, bible[book][chapter][verse])
 
 def search_key(book, chapter, kw):
     """ Keyword (kw) search on specified bible[book][chapter]
@@ -48,7 +48,7 @@ def search_booklist(bookList, kw):
     #global bible, chapsInBook
     result = []
     for book in bookList:
-        for chapter in xrange(1, chapsInBook[book]+1):
+        for chapter in range(1, chapsInBook[book]+1):
             _result = search_key(book, chapter, kw)
             result.append(_result)
     return result
@@ -86,46 +86,46 @@ def display_book(book, halt=True):
     halt at the end of each chapter 
     """
     #global bible, cbible, chapsInBook
-    for chapter in xrange(1, chapsInBook[book]+1):
-        print "%s %s" % (book, chapter)
-        for verse in xrange(1, len(bible[book][chapter])+1):
-            print "%2d  %s" % (verse, bible[book][chapter][verse])
-            print "%2d  %s\n" % (verse, cbible[book][chapter][verse])
+    for chapter in range(1, chapsInBook[book]+1):
+        print("{0} {1}".format(book, chapter))
+        for verse in range(1, len(bible[book][chapter])+1):
+            print("{0} {1}".format(verse, bible[book][chapter][verse]))
+            print("{0} {1}\n".format(verse, cbible[book][chapter][verse]))
         if halt and (chapter < chapsInBook[book]):
-            raw_input("hit any key to continue")
+            input("hit any key to continue")
 
 def display_chapter(book, chapter, halt=False):
     """ Dispaly a chapter in a book 
     """
     #global bible, cbible
-    print book, chapter
-    for verse in xrange(1, len(bible[book][chapter])+1):
-        print "%2d  %s" % (verse, bible[book][chapter][verse])
-        print "%2d  %s\n" % (verse, cbible[book][chapter][verse])
+    print(book, chapter)
+    for verse in range(1, len(bible[book][chapter])+1):
+        print ("{0} {1}".format(verse, bible[book][chapter][verse]))
+        print ("{0} {1}\n".format(verse, cbible[book][chapter][verse]))
         if halt:
-            raw_input("hit any key to continue")
+            input("hit any key to continue")
 
 def display_verse(book, chapter, verse):
     """ Dispaly a verse in the bible 
     """
     #global bible, cbible
-    print '%s\n%s:%2d\n' % (book, chapter, verse)
-    print "%s" % bible[book][chapter][verse]
-    print "%s\n" % cbible[book][chapter][verse]
+    print ("{0}\n{1}:{2}\n".format(book, chapter, verse))
+    print ("{0}".format(bible[book][chapter][verse]))
+    print ("{0}\n".format(cbible[book][chapter][verse]))
                                             
 def test0():
     """ test on global variables """
     
     #global ALLbooks, OTbooks, NTbooks, chapsInBook
-    print "\nAll books in bible:"
-    print ALLbooks
-    print "\nbooks in OT:"
-    print OTbooks
-    print "\nbooks in NT:"
-    print NTbooks
-    print "\nChapters in Books:"
+    print("\nAll books in bible:")
+    print(ALLbooks)
+    print("\nbooks in OT:")
+    print(OTbooks)
+    print("\nbooks in NT:")
+    print(NTbooks)
+    print("\nChapters in Books:")
     for book in ALLbooks:
-        print "%s : %d" % (book, chapsInBook[book])
+        print("{0} : {1}".format(book, chapsInBook[book]))
 
 
 def test1():
@@ -133,20 +133,20 @@ def test1():
     
     #global bible    
     # test to print John 3:16
-    print "\ntest to print John 3:16"
+    print("\ntest to print John 3:16")
     print(bible['John'][3][16])
     print(cbible['John'][3][16])
     
     # test of random_verse
-    print "\ntest of random_verse"
-    print random_verse(bible)
+    print("\ntest of random_verse")
+    print(random_verse(bible))
 
     # test of random_verse on book Acts
-    print "\ntest of random_verse on book Acts"    
-    print random_verse(bible, 'Acts')
+    print("\ntest of random_verse on book Acts")
+    print(random_verse(bible, 'Acts'))
     
     # test of display 1 Jone 5
-    print "\ntest of display 1 John Chapter 5"
+    print("\ntest of display 1 John Chapter 5")
     display_chapter('1 John', 5)
     display_book('James', False)
     
@@ -154,41 +154,41 @@ def test_search():
     """ test on search functions """
     
     # test of search on book John chapter 3
-    print "\ntest of search on word 'God' in John chapter 3"
+    print("\ntest of search on word 'God' in John chapter 3")
     results = search_key('John', 3, 'God')
-    print results
+    print(results)
     book, chapter, verses = results
     for verse in verses:
-        print '%s %s:%s \n%s' % (book, chapter, verse, bible[book][chapter][verse])
-        print '%s %s:%s \n%s\n' % (book, chapter, verse, cbible[book][chapter][verse])
+        print('{0} {1}:{2} \n{3}'.format(book, chapter, verse, bible[book][chapter][verse]))
+        print('{0} {1}:{2} \n{3}\n'.format (book, chapter, verse, cbible[book][chapter][verse]))
     # test of search on OT
-    print "\ntest of search on word 'what wilt thou' in OT"
+    print("\ntest of search on word 'what wilt thou' in OT")
     results = search_OT('what wilt thou')
     for piece in results:
         book, chapter, verses = piece
         for verse in verses:
-            print '%s %s:%s \n%s' % (book, chapter, verse, bible[book][chapter][verse])        
-            print '%s %s:%s \n%s\n' % (book, chapter, verse, cbible[book][chapter][verse])
+            print('{0} {1}:{2} \n{3}'.format(book, chapter, verse, bible[book][chapter][verse]))
+            print('{0} {1}:{2} \n{3}\n'.format(book, chapter, verse, cbible[book][chapter][verse]))
     # test of search on NT
-    print "\ntest of search on word 'what wilt thou' in NT"
+    print("\ntest of search on word 'what wilt thou' in NT")
     results = search_NT('what wilt thou')
     for piece in results:
         book, chapter, verses = piece
         for verse in verses:
-            print '%s %s:%s \n%s' % (book, chapter, verse, bible[book][chapter][verse])
-            print '%s %s:%s \n%s\n' % (book, chapter, verse, cbible[book][chapter][verse])
+            print('{0} {1}:{2} \n{3}'.format(book, chapter, verse, bible[book][chapter][verse]))
+            print('{0} {1}:{2} \n{3}\n'.format(book, chapter, verse, cbible[book][chapter][verse]))
             
 def quit():
     sys.exit(0)
 
 def listOTbooks():
     #global OTbooks
-    print 'Books in Old Testament:\n'
+    print('Books in Old Testament:\n')
     print(', '.join(OTbooks))
 
 def listNTbooks():
     #global NTbooks
-    print 'Books in New Testament:\n'
+    print('Books in New Testament:\n')
     print(', '.join(NTbooks))
     
 def displayText():
@@ -196,15 +196,15 @@ def displayText():
     #
     # input book
     #
-    book = raw_input("Input name of the book: ")
+    book = input("Input name of the book: ")
     if (book not in ALLbooks):
-        print '\nbook must be one of --\n%s\n' % ALLbooks
-        print random_verse(bible)
+        print("\nbook must be one of --\n{0}\n".format(ALLbooks))
+        print(random_verse(bible))
         return
     #
     # input chapter
     #
-    _tmp = raw_input("Input chapter no. in the book: ")
+    _tmp = input("Input chapter no. in the book: ")
     if (_tmp == ''):                # no chapter is entered
         display_book(book)              # display book
         return
@@ -212,13 +212,13 @@ def displayText():
         chapter = int(_tmp)         # chapter must be OK, all error goes to 1
         if (chapter > chapsInBook[book] or chapter < 1):
             if ():
-                print '\nThere is only one chapter in the book of %s.\n' % book                
+                print('\nThere is only one chapter in the book of {0}.\n'.format(book))
             else:
-                print '\nThere are %d chapters in the book of %s.\n' % (chapsInBook[book], book)
-            print random_verse(bible, book)
+                print('\nThere are {0} chapters in the book of {1}.\n'.format(chapsInBook[book], book))
+            print(random_verse(bible, book))
             return
         else:                       # chapter OK, then input verse
-            _tmp = raw_input("Input the verse no.: ")
+            _tmp = input("Input the verse no.: ")
             if (_tmp == ''):        # no verse is entered
                 display_chapter(book, chapter)  # display book+chapter
             else:                   # verse OK?
@@ -227,32 +227,32 @@ def displayText():
                     display_verse(book, chapter, verse)
                     return
                 except:             # something went wrong with the verse
-                    print '\nYour selection is not in the Bible!\n'
-                    print random_verse(bible, book)   
+                    print('\nYour selection is not in the Bible!\n')
+                    print(random_verse(bible, book))
         
 def search():
-    kw = raw_input("Input search key words: ")
-    print """
+    kw = input("Input search key words: ")
+    print("""
     Search in old testament,
               new testament, or
               all books
-    """
-    choice = raw_input("o/n/a: ")
-    print 'Search "%s" in ' % kw,
+    """)
+    choice = input("o/n/a: ")
+    print('Search "{0}" in '.format(kw))
     if (choice == 'o' or choice == 'O'):
         results = search_OT(kw)
-        print 'Old testament:'
+        print('Old testament:')
     elif (choice == 'n' or choice == 'N'):
         results = search_NT(kw)
-        print 'New testament:'
+        print('New testament:')
     else: # all other cases: including (choice == 'a' or choice == 'A'):
         results = search_ALL(kw)
-        print 'All books:'
+        print('All books:')
     for piece in results:
         book, chapter, verses = piece
         for verse in verses:
-            print '%s %s:%s \n%s' % (book, chapter, verse, bible[book][chapter][verse])
-            print '%s %s:%s \n%s\n' % (book, chapter, verse, cbible[book][chapter][verse])
+            print('{0} {1}:{2} \n{3}'.format(book, chapter, verse, bible[book][chapter][verse]))
+            print('{0} {1}:{2} \n{3}\n'.format(book, chapter, verse, cbible[book][chapter][verse]))
         
     
 def testAll():
@@ -287,18 +287,18 @@ def main():
     }
 
     while True:
-        print PROMPT
-        choice = raw_input("Your choice: ")
+        print(PROMPT) 
+        choice = input("Your choice: ")
         menu[choice]()
 
 # -----------------------------------------------------------------------------
 #
 # prepare our bible
 #                
-pkl_file = open('bible.pkl', 'r')
-bible = pickle.load(pkl_file)
-pkl_file = open('cbible.pkl', 'r')
-cbible = pickle.load(pkl_file)
+pkl_file = open('bible.pkl', 'rb')
+bible = pickle.load(pkl_file, encoding='utf-8')
+pkl_file = open('cbible.pkl', 'rb')
+cbible = pickle.load(pkl_file, encoding='utf-8')
 
 #
 # i am lazy, so let the computer construct some global variables
